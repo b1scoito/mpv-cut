fe73e7e7e5979fa1a7feefd173cfab0832478863
biscoito <biscoito@riseup.net>
2022-01-20 18:55:45 -0300

    wip: Custom FFmpeg parameters

--- mpv_cut.lua
+++ mpv_cut.lua
@@ -6,7 +6,17 @@ utils = require("mp.utils")
 -- #region globals
 local settings = {
     key_mark_cut = "c",
-    video_ext = "mp4"
+    web_key_mark_cut = "shift+c",
+
+    -- output video extension
+    video_ext = "mp4",
+    
+    -- extract only this selected audio track
+    audio_track_extract = "1",
+
+    -- web save settings
+    web_audio_target_bitrate = "128", -- kbps
+    web_video_target_file_size = "8"  -- mb
 }
 
 local var = {
@@ -14,7 +24,10 @@ local var = {
     str_dir = nil,
     str_filename = nil,
 
-    sec_duration = nil
+    sec_duration = nil,
+
+    str_ffmpeg_append = {""},
+    str_ffmpeg_c_copy = {""}
 }
 
 local pos = {
@@ -49,24 +62,37 @@ function reset_pos()
     pos.start_pos = nil
     pos.end_pos = nil
 end
+
 -- #endregion
 
-function ffmpeg_cut(time_start, time_end, input_file, output_file)
+-- #region main
+function ffmpeg_cut(time_start, time_end, input_file, output_file, is_web_save)
+    -- Do this later
+    if is_web_save then
+        local res_bitrate = ((settings.web_video_target_file_size * 8192) / var.sec_duration) - settings.web_audio_target_bitrate
+        var.str_ffmpeg_append = {"-b:v", string.format("%sk", res_bitrate),
+                                 "-b:a", string.format("%sk", settings.web_audio_target_bitrate)}
+    end
+
     mp.command_native_async({
         name = "subprocess",
         args = {"ffmpeg", "-ss", time_start, "-to", time_end, "-i", input_file, "-c", "copy", output_file}
     }, function(res, val, err)
-        if err then
-            msg.error(err)
+        if res then
             return err
         end
     end)
 
+    var.str_ffmpeg_append = {""}
+
     return nil
 end
 
--- #region main
-function mark_cut_pos()
+function web_mark_cut_pos()
+    mark_cut_pos(true)
+end
+
+function mark_cut_pos(is_web_save)
     local current_pos = mp.get_property_number("time-pos")
 
     if not pos.start_pos then
@@ -86,15 +112,18 @@ function mark_cut_pos()
     mp.osd_message(string.format("Marked %s as end position", current_pos), 5)
     
     local output_name = string.format("%s-cut.%s", str_split(var.str_filename, ".")[1], settings.video_ext)
+    
     -- Cut
-    if ffmpeg_cut(to_timestamp(pos.start_pos), to_timestamp(pos.end_pos), var.str_path, output_name) then
+    if ffmpeg_cut(to_timestamp(pos.start_pos), to_timestamp(pos.end_pos), var.str_path, output_name, is_web_save) then
         mp.osd_message("Failed to execute FFmpeg!", 10)
+        reset_pos()
+        return
     end
 
     -- Reset vars
     reset_pos()
 
-    mp.osd_message(string.format("Saved as %s successfully.", output_name), 5)
+    mp.osd_message(string.format("Saved as %s.", output_name), 5)
     
 end
 -- #endregion
@@ -109,3 +138,4 @@ mp.register_event("file-loaded", function()
 end)
 
 mp.add_key_binding(settings.key_mark_cut, "mark_cut_pos", mark_cut_pos)
+-- mp.add_key_binding(settings.web_key_mark_cut, "web_mark_cut_pos", web_mark_cut_pos)
